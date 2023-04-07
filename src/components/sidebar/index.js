d34ed89245d3d33c231d10c990cdcfbd765e9f9f
DucPhamH <ducphamhong2@gmail.com>
2023-04-07 09:48:10 +0700

    add sidebar finish

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import "./sidebar.css";
 import SidebarButton from "./sidebarButton";
+import { MdFavorite } from "react-icons/md";
+import { FaGripfire, FaPlay } from "react-icons/fa";
+import { FaSignOutAlt } from "react-icons/fa";
+import { IoLibrary } from "react-icons/io5";
+import { MdSpaceDashboard } from "react-icons/md";
 
 export default function Sidebar() {
   const [image, setImage] = useState(
@@ -10,8 +15,17 @@ export default function Sidebar() {
     <div className="sidebar-container">
       <img src={image} className="profile-img" alt="profile" />
       <div>
-        <SidebarButton />
+        <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard />} />
+        <SidebarButton title="Trending" to="/trending" icon={<FaGripfire />} />
+        <SidebarButton title="Player" to="/player" icon={<FaPlay />} />
+        <SidebarButton
+          title="Favorites"
+          to="/favorites"
+          icon={<MdFavorite />}
+        />
+        <SidebarButton title="Library" to="/" icon={<IoLibrary />} />
       </div>
+      <SidebarButton title="Sign Out" to="" icon={<FaSignOutAlt />} />
     </div>
   );
 }
--- src/screens/favorites/index.js
+++ src/screens/favorites/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Favorites() {
-  return <div>F</div>;
+  return <div className="screen-container">F</div>;
 }
