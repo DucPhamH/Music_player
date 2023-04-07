0e93db291f336d6fcee889c276f79fad6b02577b
DucPhamH <ducphamhong2@gmail.com>
2023-04-07 11:31:58 +0700

    add new url

--- src/spotify.js
+++ src/spotify.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const authEndpoint = "https://accounts.spotify.com/authorize?";
 const clientId = "6d22abd2f03b4ec2a7f032bad3f30d4c";
-const redirectUri = "http://localhost:3000";
+const redirectUri = "https://music-player-20194516.vercel.app";
 const scopes = ["user-library-read", "playlist-read-private"];
 
 export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
